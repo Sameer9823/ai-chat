fd03724f9cb1c73e886ee1679065ef68f5e89d58
Sameer Selokar <sameerselokar924@gmail.com>
2025-09-02 13:25:13 +0530

    master

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className="text-4xl font-bold text-center mt-10">Get Started with AI </h1>
+  
+   </div>
+  );
+}
